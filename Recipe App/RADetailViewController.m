5a9f8c328970468a541f7f35cb635260fa43f83d
Justin Oakes <justin@justinoak.es>
2015-04-30 15:00:22 -0600

    have correct text on tables

--- Recipe App/RADetailViewController.m
+++ Recipe App/RADetailViewController.m
@@ -24,33 +24,32 @@ static CGFloat margin = 25;
     self.title = [RARecipes titleAtIndex:self.recipeIndex];
     
     CGFloat contentSize = 0;
-    CGFloat lableY = 15;
+    CGFloat topMargin = 15;
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
     NSArray *lables = @[@"Description", @"Ingredients", @"Direction"];
-    NSArray *textArray = @[[RARecipes directionsAtIndex:self.recipeIndex], [RARecipes recipeAtIndex:self.recipeIndex][@"recipeIngredients"],[RARecipes recipeAtIndex:self.recipeIndex][@"directions"]];
     
     for (NSString *i in lables) {
-        UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, lableY, CGRectGetWidth(self.view.frame) -15, 20)];
+        UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, topMargin, CGRectGetWidth(self.view.frame) -15, 15)];
         newLabel.text = i;
         [scrollView addSubview:newLabel];
         contentSize += margin + CGRectGetHeight(newLabel.frame);
-        lableY += margin + 20;
+        topMargin += margin + 15;
         
-        NSString *textLableText = @"";
-        for (NSString *i in textArray[0]) {
+        NSString *textLableText = [self detailTextForLable:i];
+        
             textLableText = [textLableText stringByAppendingFormat:@"%@ ", i];
-        }
-
+        
         CGRect frame = [textLableText boundingRectWithSize:CGSizeMake(CGRectGetWidth(self.view.frame) - 25, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil context:nil];
-        frame.origin = CGPointMake(25, lableY);
+        frame.origin = CGPointMake(25, topMargin);
         UILabel *textLabel= [[UILabel alloc] initWithFrame:frame];
-
+        
+        NSLog(@"%f %f   %f   %f",frame.origin.x, frame.origin.y, frame.size.height, frame.size.width );
         [textLabel setNumberOfLines:0];
         textLabel.text = textLableText;
         [scrollView addSubview:textLabel];
         contentSize += margin + CGRectGetHeight(textLabel.frame);
-        lableY +=  margin + CGRectGetHeight(textLabel.frame);
+        topMargin +=  margin + CGRectGetHeight(textLabel.frame);
    
    }
     scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.view.frame), contentSize);
@@ -63,7 +62,24 @@ static CGFloat margin = 25;
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
+-(NSString *)detailTextForLable:(NSString *)lable{
+    if ([lable isEqualToString:@"Description"]) {
+        return [RARecipes descriptionAtIndex:self.recipeIndex];
+    }else if ([lable isEqualToString:@"Ingredients"]){
+        return [RARecipes ingredientTypeAtIndex:self.recipeIndex inRecipeAtIndex:self.recipeIndex];
+    }else{
+        NSString *directionsString = @"";
+        for (NSString *part in [RARecipes directionsAtIndex:self.recipeIndex]) {
+            [directionsString stringByAppendingString:part];
+        }
+        return  directionsString;
+    }
+    return @"NO VALUE";
+}
+    
+
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -71,6 +87,6 @@ static CGFloat margin = 25;
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
 
 @end
