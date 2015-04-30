035465d742f5e6ab53f4a8af2101320e2f7ca3ac
Doug Voss <douglas.c.voss@gmail.com>
2015-04-30 16:48:38 -0600

    initial commit

--- Recipe App/RADetailViewController.m
+++ Recipe App/RADetailViewController.m
@@ -15,7 +15,47 @@
 
 @implementation RADetailViewController
 
-static CGFloat margin = 25;
+static const CGFloat xMargin = 15;
+static const CGFloat interItemMargin = 0;
+
+- (CGFloat)heightOfReferenceString:(NSString *)reference {
+    
+    CGRect bounding = [reference boundingRectWithSize:CGSizeMake(self.view.frame.size.width - 2 * xMargin, 0)
+                                              options:NSStringDrawingUsesLineFragmentOrigin
+                                           attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15]}
+                                              context:nil];
+    
+    return bounding.size.height;
+    
+}
+
+- (CGFloat)heightOfCell:(NSString *)text
+     withSuperviewWidth:(CGFloat)superviewWidth
+{
+    CGFloat labelWidth                  = superviewWidth - 30.0f;
+    //    use the known label width with a maximum height of 100 points
+    CGSize labelContraints              = CGSizeMake(labelWidth, 100.0f);
+    
+    NSStringDrawingContext *context     = [[NSStringDrawingContext alloc] init];
+    
+    CGRect labelRect                    = [text boundingRectWithSize:labelContraints
+                                                                       options:NSStringDrawingUsesLineFragmentOrigin
+                                                                    attributes:nil
+                                                                       context:context];
+    
+    //    return the calculated required height of the cell considering the label
+    return labelRect.size.height;
+}
+
+- (CGFloat)heightForText:(NSString*)text font:(UIFont*)font withinWidth:(CGFloat)width {
+    CGSize size = [text sizeWithAttributes:@{NSFontAttributeName:font}];
+    NSLog(@"size.width = %f; size.height = %f", size.width, size.height);
+    CGFloat area = size.height * size.width;
+    CGFloat height = roundf(area / (width));
+    NSLog(@"height = %f", height);
+    return ceilf(height / font.lineHeight) * font.lineHeight;
+    //return ceilf(height);
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,36 +63,56 @@ static CGFloat margin = 25;
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = [RARecipes titleAtIndex:self.recipeIndex];
     
-    CGFloat contentSize = 0;
-    CGFloat topMargin = 15;
+    CGFloat topMargin = 0;
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
-    NSArray *lables = @[@"Description", @"Ingredients", @"Direction"];
+    CGRect scrollViewFrame = CGRectMake(0, topMargin, self.view.frame.size.width, self.view.frame.size.height);
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:scrollViewFrame];
+    NSArray *lables = @[@"Description", @"Ingredients", @"Directions"];
     
+    NSString *textLableText = nil;
     for (NSString *i in lables) {
-        UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, topMargin, CGRectGetWidth(self.view.frame) -15, 15)];
+        UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectMake(xMargin, topMargin, CGRectGetWidth(self.view.frame) - 2*xMargin, [self heightOfReferenceString:textLableText])];
         newLabel.text = i;
+        newLabel.backgroundColor = [UIColor blueColor];
+        newLabel.numberOfLines = 0;
+        [newLabel sizeToFit];
         [scrollView addSubview:newLabel];
-        contentSize += margin + CGRectGetHeight(newLabel.frame);
-        topMargin += margin + 15;
+        topMargin += CGRectGetHeight(newLabel.frame);
         
-        NSString *textLableText = [self detailTextForLable:i];
+        textLableText = nil;
+        textLableText = [self detailTextForLable:i];
         
-            textLableText = [textLableText stringByAppendingFormat:@"%@ ", i];
+        //textLableText = [textLableText stringByAppendingFormat:@"%@ ", i];
         
-        CGRect frame = [textLableText boundingRectWithSize:CGSizeMake(CGRectGetWidth(self.view.frame) - 25, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil context:nil];
-        frame.origin = CGPointMake(25, topMargin);
-        UILabel *textLabel= [[UILabel alloc] initWithFrame:frame];
+        NSLog(@"\n\ntextLableText=%@\n\n", textLableText);
+        //CGRect frame = [textLableText boundingRectWithSize:CGSizeMake(CGRectGetWidth(self.view.frame) - 25, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil context:nil];
+        //frame.origin = CGPointMake(25, topMargin);
+    
         
-        NSLog(@"%f %f   %f   %f",frame.origin.x, frame.origin.y, frame.size.height, frame.size.width );
-        [textLabel setNumberOfLines:0];
+        
+        UILabel *textLabel= [[UILabel alloc] init];
+        
+        //[textLabel setNumberOfLines:0];
+        //textLabel.text = textLableText;
+        
+        textLabel.numberOfLines = 0; // allows label to have as many lines as needed
         textLabel.text = textLableText;
+        //[textLabel sizeToFit];
+        //textLabel.frame = CGRectMake(15, topMargin, self.view.frame.size.width - 2*15, [self heightOfCell:textLabel.text withSuperviewWidth:(self.view.frame.size.width - 2*15)]);
+        //textLabel.frame = CGRectMake(xMargin, topMargin, self.view.frame.size.width - 2*xMargin, [self heightForText:textLabel.text font:textLabel.font withinWidth:self.view.frame.size.width]);
+        textLabel.frame = CGRectMake(xMargin, topMargin, self.view.frame.size.width - 2*xMargin, [self heightOfReferenceString:textLabel.text]);
+        
+        NSLog(@"textlabel.text=%@", textLabel.text);
+        NSLog(@"Label's frame is: %f, %f, %f, %f", textLabel.frame.origin.x, textLabel.frame.origin.y, textLabel.frame.size.width, textLabel.frame.size.height);
+        
+        textLabel.backgroundColor = [UIColor redColor];
+        textLabel.numberOfLines = 0;
+        [textLabel sizeToFit];
         [scrollView addSubview:textLabel];
-        contentSize += margin + CGRectGetHeight(textLabel.frame);
-        topMargin +=  margin + CGRectGetHeight(textLabel.frame);
+        topMargin +=  interItemMargin + CGRectGetHeight(textLabel.frame);
    
    }
-    scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.view.frame), contentSize);
+    scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.view.frame), topMargin);
     [self.view addSubview:scrollView];
 
 }
@@ -67,11 +127,19 @@ static CGFloat margin = 25;
     if ([lable isEqualToString:@"Description"]) {
         return [RARecipes descriptionAtIndex:self.recipeIndex];
     }else if ([lable isEqualToString:@"Ingredients"]){
-        return [RARecipes ingredientTypeAtIndex:self.recipeIndex inRecipeAtIndex:self.recipeIndex];
+        
+        NSString *catString = @"";
+        int thresh = [RARecipes ingredientCountAtIndex:self.recipeIndex];
+        for (int i=0; i<thresh; i++) {
+            catString = [catString stringByAppendingString:[NSString stringWithFormat:@"%@ %@\n", [RARecipes ingredientTypeAtIndex:i inRecipeAtIndex:self.recipeIndex], [RARecipes ingredientVolumeAtIndex:i inRecipeAtIndex:self.recipeIndex]]];
+        }
+        // get rid of last \n character
+        return [catString substringToIndex:[catString length]-1];
+        
     }else{
         NSString *directionsString = @"";
         for (NSString *part in [RARecipes directionsAtIndex:self.recipeIndex]) {
-            [directionsString stringByAppendingString:part];
+            directionsString = [directionsString stringByAppendingString:part];
         }
         return  directionsString;
     }
